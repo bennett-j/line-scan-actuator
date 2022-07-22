97a35f5a497c63ac0e3bde0f25848b411b740744
bennett-j <53816688+bennett-j@users.noreply.github.com>
2022-07-22 17:42:05 +0100

    Added most stepper functionality

--- Firmware/WebActuatorControl/WebActuatorControl.ino
+++ Firmware/WebActuatorControl/WebActuatorControl.ino
@@ -24,18 +24,18 @@ https://marketplace.visualstudio.com/items?itemName=vsciot-vscode.vscode-arduino
 #include <AccelStepper.h>
 #include <TMCStepper.h> //TODO: implement this functionality
 
-
 //=======================//
 //  Constants & Globals  //
 //=======================//
 
 // Network details
-const char* ssid = "ESP32_AccessPoint";
-const char* password = "letMe1nPlz";
+const char *ssid = "ESP32_AccessPoint";
+const char *password = "letMe1nPlz";
 
 const int HTTP_PORT = 80;
 
-enum Status {
+enum Status
+{
     DISABLE,
     HOMING_OUT,
     HOMING_IN,
@@ -55,6 +55,7 @@ const int IDLE_LIM_PIN = 0;
 
 const int ACCEL = mm2step(1000); // I think I can use this function here
 const int HOME_SPEED = mm2step(10);
+const int TRAVEL_SPEED = mm2step(20);
 
 int velocity = 8;
 int start_pos = 0;
@@ -69,51 +70,54 @@ AsyncWebSocket ws("/ws");
 
 AccelStepper stepper = AccelStepper(stepper.DRIVER, STEP_PIN, DIR_PIN);
 
-
 //=============//
 //  Functions  //
 //=============//
 
 // Send stepper to end position then home position
-void startHomeStepper() {
+void startHomeStepper()
+{
     status = HOMING_OUT;
     // before first homing, stepper is disabled
     stepper.enableOutputs();
-    
+
     stepper.setMaxSpeed(HOME_SPEED);
     // start move plenty of steps to idle end
-    stepper.move(mm2step(2*1200));
- 
+    stepper.move(mm2step(2 * 1200));
 }
 
-int mm2step(int mm) {
+int mm2step(int mm)
+{
     return mm * steps_per_mm;
 }
 
-int step2mm(int step) {
-    return step / steps_per_mm; //TODO: deal with rounding
+int step2mm(int step)
+{
+    return step / steps_per_mm; // TODO: deal with rounding
 }
 
 // WiFi event handler to provide notifications via Serial
-void WiFiEvent(WiFiEvent_t event){
+void WiFiEvent(WiFiEvent_t event)
+{
     Serial.printf("[WiFi-event] event: %d\n", event);
 
-    switch (event) {
-        case SYSTEM_EVENT_AP_STACONNECTED:
-            Serial.println("Client connected");
-            break;
-        
-        case SYSTEM_EVENT_AP_STADISCONNECTED:
-            Serial.println("Client disconnected");
-            break;
-    
-        default:
-            break;
+    switch (event)
+    {
+    case SYSTEM_EVENT_AP_STACONNECTED:
+        Serial.println("Client connected");
+        break;
+
+    case SYSTEM_EVENT_AP_STADISCONNECTED:
+        Serial.println("Client disconnected");
+        break;
+
+    default:
+        break;
     }
 }
 
-
-void sendReport() {
+void sendReport()
+{
     DynamicJsonDocument doc(1024);
 
     doc["type"] = "report";
@@ -125,86 +129,119 @@ void sendReport() {
     String output;
     serializeJson(doc, output);
     Serial.println(output);
-  
+
     ws.textAll(output);
 }
 
-void handleWebSocketMessage(void *arg, uint8_t *data, size_t len) {
-  AwsFrameInfo *info = (AwsFrameInfo*)arg;
-  if (info->final && info->index == 0 && info->len == len && info->opcode == WS_TEXT) {
-    
-    //  >data< is the pointer 
-    // >*data< dereferences to yield the value
+void handleWebSocketMessage(void *arg, uint8_t *data, size_t len)
+{
+    AwsFrameInfo *info = (AwsFrameInfo *)arg;
+    if (info->final && info->index == 0 && info->len == len && info->opcode == WS_TEXT)
+    {
 
-    
+        //  >data< is the pointer
+        // >*data< dereferences to yield the value
 
-    //Serial.println(msg);
+        // https://arduinojson.org/
+        DynamicJsonDocument doc(1024);
+        deserializeJson(doc, (char *)data);
+        // nb (char*)data casts it to datatype char (not uint8_t)
 
-    //https://arduinojson.org/
-    DynamicJsonDocument doc(1024);
-    deserializeJson(doc, (char*)data);
-    // nb (char*)data casts it to datatype char (not uint8_t)
+        if (strcmp(doc["type"], "settings") == 0)
+        {
+            Serial.println("it's a settings message");
+            velocity = doc["velocity"];
+            start_pos = doc["start_pos"];
+            end_pos = doc["end_pos"];
+        }
+        else if (strcmp(doc["type"], "button") == 0)
+        {
+            /* TODO: check which actions are permitted. 
+             * Definitely need some validation on whether start and stop are within bounds
+             */
+            if (strcmp(doc["action"], "stp") == 0)
+            {
+                stepper.stop();
+            }
+            else if (strcmp(doc["action"], "home") == 0)
+            {
+                startHomeStepper();
+            }
+            else if (strcmp(doc["action"], "goHome") == 0)
+            {
+                stepper.setMaxSpeed(TRAVEL_SPEED);
+                stepper.moveTo(0);
+            }
+            else if (strcmp(doc["action"], "goStart") == 0)
+            {
+                stepper.setMaxSpeed(TRAVEL_SPEED);
+                stepper.moveTo(mm2step(start_pos));
+            }
+            else if (strcmp(doc["action"], "start") == 0)
+            {
+                stepper.setMaxSpeed(velocity);
+                stepper.moveTo(mm2step(end_pos));
+            }
+        }
 
-    if (strcmp(doc["type"], "settings") == 0) {
-        Serial.println("it's a settings message");
-        velocity = doc["velocity"];
-        start_pos = doc["start_pos"];
-        end_pos = doc["end_pos"];
+        sendReport();
     }
-    sendReport();
-    
-    
-  }
 }
 
 void onEvent(AsyncWebSocket *server, AsyncWebSocketClient *client, AwsEventType type,
-             void *arg, uint8_t *data, size_t len) {
-  switch (type) {
+             void *arg, uint8_t *data, size_t len)
+{
+    switch (type)
+    {
     case WS_EVT_CONNECT:
-      Serial.printf("WebSocket client #%u connected from %s\n", client->id(), client->remoteIP().toString().c_str());
-      sendReport();
-      break;
+        Serial.printf("WebSocket client #%u connected from %s\n", client->id(), client->remoteIP().toString().c_str());
+        sendReport();
+        break;
     case WS_EVT_DISCONNECT:
-      Serial.printf("WebSocket client #%u disconnected\n", client->id());
-      break;
+        Serial.printf("WebSocket client #%u disconnected\n", client->id());
+        break;
     case WS_EVT_DATA:
-      handleWebSocketMessage(arg, data, len);
-      break;
+        handleWebSocketMessage(arg, data, len);
+        break;
     case WS_EVT_PONG:
     case WS_EVT_ERROR:
-      break;
-  }
+        break;
+    }
 }
 
 // Callback: send homepage
-void onIndexRequest(AsyncWebServerRequest *request) {
-  IPAddress remote_ip = request->client()->remoteIP();
-  Serial.println("[" + remote_ip.toString() +
-                  "] HTTP GET request of " + request->url());
-  request->send(SPIFFS, "/index.html", "text/html");
+void onIndexRequest(AsyncWebServerRequest *request)
+{
+    IPAddress remote_ip = request->client()->remoteIP();
+    Serial.println("[" + remote_ip.toString() +
+                   "] HTTP GET request of " + request->url());
+    request->send(SPIFFS, "/index.html", "text/html");
 }
 
 // Callback: send style sheet
-void onCSSRequest(AsyncWebServerRequest *request) {
-  IPAddress remote_ip = request->client()->remoteIP();
-  Serial.println("[" + remote_ip.toString() +
-                  "] HTTP GET request of " + request->url());
-  request->send(SPIFFS, "/style.css", "text/css");
+void onCSSRequest(AsyncWebServerRequest *request)
+{
+    IPAddress remote_ip = request->client()->remoteIP();
+    Serial.println("[" + remote_ip.toString() +
+                   "] HTTP GET request of " + request->url());
+    request->send(SPIFFS, "/style.css", "text/css");
 }
 
 // Callback: send 404 if requested file does not exist
-void onPageNotFound(AsyncWebServerRequest *request) {
-  IPAddress remote_ip = request->client()->remoteIP();
-  Serial.println("[" + remote_ip.toString() +
-                  "] HTTP GET request of " + request->url());
-  request->send(404, "text/plain", "Not found");
+void onPageNotFound(AsyncWebServerRequest *request)
+{
+    IPAddress remote_ip = request->client()->remoteIP();
+    Serial.println("[" + remote_ip.toString() +
+                   "] HTTP GET request of " + request->url());
+    request->send(404, "text/plain", "Not found");
 }
 
 //=========//
 //  SETUP  //
 //=========//
 
-void setup(){
+void setup()
+{
     // Serial for debugging
     Serial.begin(115200);
 
@@ -213,9 +250,11 @@ void setup(){
     digitalWrite(LED_PIN, LOW);
 
     // Make sure we can read the file system
-    if( !SPIFFS.begin()){
+    if (!SPIFFS.begin())
+    {
         Serial.println("Error mounting SPIFFS");
-        while(1);
+        while (1)
+            ;
     }
 
     // Connect WiFi event handler
@@ -247,7 +286,7 @@ void setup(){
     // setup stepper
     pinMode(HOME_LIM_PIN, INPUT_PULLUP);
     pinMode(IDLE_LIM_PIN, INPUT_PULLUP);
-    
+
     stepper.setAcceleration(mm2step(ACCEL));
     // wait to set speed and enable outputs in homing
 }
@@ -256,40 +295,53 @@ void setup(){
 //  LOOP  //
 //========//
 
-void loop() {
+void loop()
+{
     ws.cleanupClients();
-    
+
     switch (status)
     {
     case HOMING_OUT:
-        if (digitalRead(IDLE_LIM_PIN) == LOW) {
+        if (digitalRead(IDLE_LIM_PIN) == LOW)
+        {
             // has reached idle end
             // temporarily save current pos but not right because we don't know start
             maxSteps = stepper.currentPosition();
             status = HOMING_IN;
         }
-        else{
+        else
+        {
             stepper.run();
         }
         break;
 
     case HOMING_IN:
-        if (digitalRead(HOME_LIM_PIN) == LOW) {
+        if (digitalRead(HOME_LIM_PIN) == LOW)
+        {
             // has reached idle end
-            
+
             // set current position to 0 and save max position
             int tmpMax = maxSteps;
             int nowPos = stepper.currentPosition();
             maxSteps = tmpMax - nowPos;
             stepper.setCurrentPosition(0);
-            
+
             // now homed, set status to IDLE
             status = IDLE;
         }
-        else{
+        else
+        {
             stepper.run();
         }
+        break;
     
+    case IDLE: // no break
+    case MOVING:
+        // this is simple - perhaps overly simple (and inefficient) and may evolve with project
+        status = stepper.run() ? MOVING : IDLE;
+        // TODO: check limit pins
+        break;
+
     default:
         break;
     }
