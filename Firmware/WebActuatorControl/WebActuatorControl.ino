bec43e868792fd3a79e52b6146127750d40b15c7
bennett-j <53816688+bennett-j@users.noreply.github.com>
2022-08-09 12:13:05 +0100

    Update lim switch pins to ones with pullup res

--- Firmware/WebActuatorControl/WebActuatorControl.ino
+++ Firmware/WebActuatorControl/WebActuatorControl.ino
@@ -57,8 +57,8 @@ const int LED_PIN = 2;
 const int STEP_PIN = 18;
 const int DIR_PIN = 5;
 const int ENABLE_PIN = 23;
-const int HOME_LIM_PIN = 34;
-const int IDLE_LIM_PIN = 35;
+const int HOME_LIM_PIN = 33;
+const int IDLE_LIM_PIN = 32;
 
 const int steps_per_mm = 50;
 
